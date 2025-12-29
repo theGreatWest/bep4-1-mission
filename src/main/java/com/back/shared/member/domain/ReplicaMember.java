46d017b3de57d73cfc571274b1f1d37a04a44ea6
youngjuKim <kjuzoojuk@naver.com>
2025-12-29 19:59:50 +0900

    feat: mission 0019

--- src/main/java/com/back/shared/member/domain/ReplicaMember.java
+++ src/main/java/com/back/shared/member/domain/ReplicaMember.java
@@ -4,13 +4,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Getter
-@Setter
 @NoArgsConstructor
 public abstract class ReplicaMember extends BaseMember {
     @Id
@@ -18,7 +16,10 @@ public abstract class ReplicaMember extends BaseMember {
     private LocalDateTime createDate;
     private LocalDateTime modifyDate;
 
-    public ReplicaMember(String username, String password, String nickname) {
+    public ReplicaMember(int id, LocalDateTime createDate, LocalDateTime modifyDate, String username, String password, String nickname) {
         super(username, password, nickname);
+        this.id = id;
+        this.createDate = createDate;
+        this.modifyDate = modifyDate;
     }
 }
